ce0ab03ff86e22e10b03099d1798d19358b45b27
Shetty12698 <shetty.shrey@northeastern.edu>
2022-10-26 14:31:31 -0500

    patient directory and patient

---
+++ Hospital management system/src/hospital/management/system/patient.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospital.management.system;
+
+/**
+ *
+ * @author SHREYAS
+ */
+public class patient {
+    private encounterhistory encounterHistory;
+    
+    public patient(encounterhistory encounterHistory) {
+        this.encounterHistory = encounterHistory;
+        
+    }
+
+    public encounterhistory getEncounterHistory() {
+        return encounterHistory;
+    }
+
+    public void setEncounterHistory(encounterhistory encounterHistory) {
+        this.encounterHistory = encounterHistory;
+    }
+    
+        
+    }
+    
+    
+        
+    
+    
+    
+       
+    
+    
+
