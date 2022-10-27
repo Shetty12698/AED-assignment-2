1f26aaa230d0a827ae838365a673e5c7e672222d
Shetty12698 <shetty.shrey@northeastern.edu>
2022-10-27 13:55:20 -0500

    house added

--- Hospital management system/src/hospital/management/system/house.java
+++ Hospital management system/src/hospital/management/system/house.java
@@ -3,11 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package hospital.management.system;
+import java.util.ArrayList;
 
 /**
  *
  * @author SHREYAS
  */
 public class house {
+    private String housename;
+    private ArrayList<person> persons;
+    
+    
+    public house(String housename, ArrayList<person> persons) {
+        this.housename = housename;
+        this.persons = persons;
+    }
+
+    public String getHousename() {
+        return housename;
+    }
+
+    public void setHousename(String housename) {
+        this.housename = housename;
+    }
+
+    public ArrayList<person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<person> persons) {
+        this.persons = persons;
+    }
+    
+    
+
     
 }
