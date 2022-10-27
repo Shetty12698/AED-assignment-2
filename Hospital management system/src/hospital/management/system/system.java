c33936d70cb0d19a7bf77c3cc34d6b5407ea08c7
Shetty12698 <shetty.shrey@northeastern.edu>
2022-10-27 14:48:41 -0500

    system added

--- Hospital management system/src/hospital/management/system/system.java
+++ Hospital management system/src/hospital/management/system/system.java
@@ -3,12 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package hospital.management.system;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author SHREYAS
  */
 public class system {
+    private patientdirectory Patientdirectory;
+    private persondirectory Persondirecory;
+    private ArrayList<city> cities;
+    
+    public system(patientdirectory Patientdirectory, persondirectory Persondirectory, ArrayList<city> cities) {
+        this.Patientdirectory = Patientdirectory;
+        this.Persondirectory = Persondirectory;
+        this.cities = cities;
+        
+    }
+
+    public patientdirectory getPatientdirectory() {
+        return Patientdirectory;
+    }
+
+    public void setPatientdirectory(patientdirectory Patientdirectory) {
+        this.Patientdirectory = Patientdirectory;
+    }
+
+    public persondirectory getPersondirecory() {
+        return Persondirecory;
+    }
+
+    public void setPersondirecory(persondirectory Persondirecory) {
+        this.Persondirecory = Persondirecory;
+    }
+
+    public ArrayList<city> getCities() {
+        return cities;
+    }
+
+    public void setCities(ArrayList<city> cities) {
+        this.cities = cities;
+    }
+    
+    
     
     
 }
